929d45e63e02b06a174ff12782133e3e84cc4f56
Anh Pham <phamduyanh249@live.com>
2018-07-07 23:53:11 +1000

    Added basic styling for add new class form in web-app. Also added updating ability for the form when input changes.

--- web-app/src/components/contents/Teacher/classroom/components/NoClass.js
+++ web-app/src/components/contents/Teacher/classroom/components/NoClass.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { Modal, Button } from "antd";
+import { Modal, Button, Input, Select } from "antd";
 
 export default class NoClass extends React.Component {
   state = {
-    visible: false
+    newClassFormVisible: false
   };
 
   showAddClassroomForm = () => {
     this.setState({
-      visible: true
+      newClassFormVisible: true
     });
   };
 
-  handleAdd = e => {
-    console.log(e);
+  handleAdd = classInfo => {
+    console.log(classInfo);
     this.setState({
-      visible: false
+      newClassFormVisible: false
     });
   };
 
   handleCancel = e => {
     console.log(e);
     this.setState({
-      visible: false
+      newClassFormVisible: false
     });
   };
 
@@ -30,29 +30,109 @@ export default class NoClass extends React.Component {
     return (
       <div style={{ padding: "80px 100px", textAlign: "center" }}>
         <div className="NoClass">
-          <p>Looks like you are not managing any classrooms... yet.</p>
-          <Button size="large" type="primary" onClick={this.showAddClassroomForm}>
+          <p id="heading">
+            Looks like you are not managing any classrooms... yet.
+          </p>
+          <Button
+            size="large"
+            type="primary"
+            onClick={this.showAddClassroomForm}
+          >
             Create your first new classroom
           </Button>
           <div>
             <img src="/classroomIcon.png" />
           </div>
+          <NewClassForm
+            visible={this.state.newClassFormVisible}
+            handleAdd={this.handleAdd}
+            handleCancel={this.handleCancel}
+          />
         </div>
-        <NewClassForm visible={this.state.visible} handleAdd={this.handleAdd} handleCancel={this.handleCancel}/>
       </div>
     );
   }
 }
 
-const NewClassForm = props => (
-  <Modal
-    title="New classroom"
-    visible={props.visible}
-    onOk={props.handleAdd}
-    onCancel={props.handleCancel}
-  >
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-  </Modal>
+class NewClassForm extends React.Component {
+  state = {
+    className: "",
+    classDescription: "",
+    classFalcuty: "0"
+  };
+
+  handleInputChange = fieldChange => e => {
+    // because select tag has value directly, so has to use condition to get only value of e when e.target exists
+    if (e.target) {
+      this.setState({ [fieldChange]: e.target.value });
+    } else {
+      this.setState({ [fieldChange]: e });
+    }
+  };
+
+  render() {
+    return (
+      <Modal
+        className="NewClassForm"
+        title="New classroom"
+        visible={this.props.visible}
+        onOk={() => this.props.handleAdd(this.state)}
+        onCancel={this.props.handleCancel}
+        okText="Add"
+      >
+        <div>
+          <p>Class name</p>
+          <Input
+            onChange={this.handleInputChange("className")}
+            placeholder="Classroom name"
+          />
+        </div>
+        <div>
+          <p>Description</p>
+          <Input.TextArea
+            onChange={this.handleInputChange("classDescription")}
+            placeholder="The class is about..."
+          />
+        </div>
+        <div style={{ marginBottom: "20px" }}>
+          <LinePicker onChange={this.handleInputChange("classFalcuty")} />
+        </div>
+        <div>
+          <FalcutyPicker onChange={this.handleInputChange("classFalcuty")} />
+        </div>
+      </Modal>
+    );
+  }
+}
+
+const LinePicker = props => (
+  <div className="Picker">
+    <p>Line:</p>
+    <Select onChange={props.onChange} defaultValue="0">
+      <Select.Option value="1">1</Select.Option>
+      <Select.Option value="2">2</Select.Option>
+      <Select.Option value="3">3</Select.Option>
+      <Select.Option value="4">4</Select.Option>
+      <Select.Option value="5">5</Select.Option>
+      <Select.Option value="6">6</Select.Option>
+      <Select.Option value="7">7</Select.Option>
+      <Select.Option value="8">8</Select.Option>
+      <Select.Option value="9">9</Select.Option>
+    </Select>
+  </div>
+);
+
+const FalcutyPicker = props => (
+  <div className="Picker">
+    <p>Falcuty: </p>
+    <Select onChange={props.onChange} defaultValue="0">
+      <Select.Option value="0">Arts</Select.Option>
+      <Select.Option value="1">English and Humanities</Select.Option>
+      <Select.Option value="2">Languages</Select.Option>
+      <Select.Option value="3">Mathematics</Select.Option>
+      <Select.Option value="4">Physical and Outdoor Education</Select.Option>
+      <Select.Option value="5">Social and Behavioural Sciences</Select.Option>
+      <Select.Option value="6">Technology and Design</Select.Option>
+    </Select>
+  </div>
 );
